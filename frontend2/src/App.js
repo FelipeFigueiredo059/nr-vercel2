37a01ab8297ddb77b155f00d5f86d8595ec23f07
FelipeFigueiredo059 <felipefigueiredo043@gmail.com>
2023-06-10 19:45:32 -0300

    frontend

---
+++ frontend2/src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import LoginPage from "./pages/login/LoginPage";
+import HomePage from "./pages/home/Home";
+import List from "./pages/List/List";
+import Password from "./pages/password/password";
+import PasswordReset from "./pages/passwordReset/PasswordReset";
+import Cursos from "./pages/Curso/Curso";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/employees" element={<List />} />
+        <Route path="/senha" element={<Password />} />
+        <Route path="/cursos" element={<Cursos />} />
+        <Route
+          path="/api/password-reset/:id/:token"
+          element={<PasswordReset />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend2/src/pages/List/List.js
@@ -0,0 +1,40 @@
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function List() {
+  const navigateTo = useNavigate();
+
+  useEffect(() => {
+    const accessToken = sessionStorage.getItem("accessToken");
+
+    if (!accessToken) {
+      navigateTo("/");
+    }
+  }, [navigateTo]);
+
+  const [listOfEmployees, setListOfEmployees] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://sondaengenhariamds.sa-east-1.elasticbeanstalk.com/register")
+      .then((response) => {
+        setListOfEmployees(response.data);
+      });
+  }, []);
+
+  return (
+    <div>
+      {listOfEmployees.map((value, key) => {
+        return (
+          <div className="post">
+            <div className="name">{value.name}</div>
+            <div className="cpf">{value.cpf}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default List;
